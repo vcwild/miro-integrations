5db89df9946d47fc993f1a4ce87dc23df2939ab7
vcwild <vcwildm@gmail.com>
2021-05-07 00:04:55 -0300

    update environment

--- src/env.py
+++ src/env.py
@@ -1,9 +1,14 @@
 import dotenv
 import os
+import httpx
 
 dotenv.load_dotenv()
 
-MIRO_AUTH_TOKEN = os.environ.get("MIRO_AUTH_TOKEN")
+CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
+MIRO_AUTH_TOKEN = os.environ["MIRO_AUTH_TOKEN"]
 BOARD_ID = os.environ['BOARD_ID']
 USER_ID = os.environ['USER_ID']
 TEAM_ID = os.environ['TEAM_ID']
+
+SLACK_WEBHOOK_URL= os.environ['SLACK_WEBHOOK_URL']
