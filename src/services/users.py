2852f838a251ae47d034fc75c762eea2dc84b246
vcwild <vcwildm@gmail.com>
2021-05-18 00:54:49 -0300

    update services

--- src/services/users.py
+++ src/services/users.py
@@ -1,14 +1,19 @@
 from collections import Counter
+from itertools import groupby
 from datetime import datetime, timedelta
 from typing import Dict
 from conversors import (
 	from_system_time_to_gmt,
 	str_to_datetime
 )
-from filters import filter_keys
+from filters import filter_html, filter_keys
 from miro.client import Client
 
-def get_user_activity_by_widget_type(client: Client, board_id: str, from_minutes: int, type: str='sticker'):
+def get_user_activity_by_widget_type(
+	client: Client,
+	board_id: str,
+	from_minutes: int,
+	type: str='sticker'):
 	board = client.list_all_widgets(board_id)
 	items = [item for item in board['data'] if item['type'] in type]
 
@@ -64,3 +69,50 @@ def get_user_activity(client: Client, board_id: str, from_minutes: int) -> Dict:
 
 	return users
 
+def get_user_data_with_text_fields(client: Client, board_id: str, from_minutes: int) -> Dict:
+	board = client.list_all_widgets(board_id)
+
+	types = ['text', 'sticker']
+
+	board_data = []
+	for item in board['data']:
+		if item['type'] in types:
+			item_with_datetime = str_to_datetime(item, ['modifiedAt'])
+
+			filtered_dict = filter_keys(
+				item_with_datetime,
+				['modifiedAt', 'modifiedBy', 'text']
+			)
+			filtered_dict['text'] = filter_html(filtered_dict['text'])
+			filtered_dict['user'] = filtered_dict['modifiedBy']['name']
+			filtered_dict['user_id'] = filtered_dict['modifiedBy']['id']
+
+			filtered_dict.pop('modifiedBy')
+			board_data.append(filtered_dict)
+
+	time_gmt_minus_n_minutes = \
+		from_system_time_to_gmt(datetime.now()) - timedelta(minutes=from_minutes)
+
+	board_data = list(filter(
+		lambda x: x['modifiedAt'] >= time_gmt_minus_n_minutes , board_data
+	))
+
+	return board_data
+
+def get_user_reports(user_activity):
+	sorted_user_activity = sorted(user_activity, key=lambda x: x['modifiedAt'], reverse=False)
+
+	grouped_data = groupby(sorted_user_activity, key=lambda x: x['user'])
+
+	users = {}
+	for key, group in grouped_data:
+		users[str(key)] = list(group)
+
+	user_reports = {}
+	for user_k, user_v in users.items():
+		user_text = []
+		for element in user_v:
+			user_text.append(element['text'])
+		user_reports[str(user_k)] = user_text
+	
+	return user_reports
