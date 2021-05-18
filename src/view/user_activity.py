0e32b2e65dddd734e31215ff16b130c902e9cbd4
vcwild <vcwildm@gmail.com>
2021-05-18 00:55:13 -0300

    create view for user reports

--- src/view/user_activity.py
+++ src/view/user_activity.py
@@ -1,16 +1,28 @@
 def show_user_activity(
 	user_activity,
 	from_minutes,
-	message="`{}` realizou `{}` atividades nos boards nos últimos `{}` minutos."):
+	message="`{}` `{}` `{}`."):
 
 	if user_activity == {}:
 		raise TypeError("No user activity recorded")
 
 	messages = ""
-
 	for user, activities in user_activity.items():
 		message = message.format(user, activities, from_minutes)
 		messages += "\n"
+		messages += message
+
+	body = {
+		'text': messages
+	}
+
+	return body
+
+def show_user_reports(user_reports, from_minutes):
+	messages = ""
+	for user, values in user_reports.items():
+		str_values = ", ".join([str(item) for item in values])
+		message = "**{}** publicou há {} minutos: {}".format(user, from_minutes, str_values)
 		messages += message
 
 	body = {
